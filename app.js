024b696e9787929b7308be54366a9930f1ba74bc
Dasol Park <dasol.park615@gmail.com>
2020-02-02 21:55:45 +0900

    Middlewares Done

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 import express from 'express';
+import helmet from 'helmet';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const PORT = 4000;
 
+const onHomeListening = (req, res) => {
+  console.log(req.cookies);
+  res.send('Hi Home!');
+};
+
 const onServerListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 };
 
-const onHomeListening = (req, res) => {
-  res.send('Hi Home!');
-};
+app.use(helmet());
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', onHomeListening);
 
