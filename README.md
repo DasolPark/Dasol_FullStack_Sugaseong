4857adf5a5b9a1154b5623494c6fdd8e2769a943
Dasol Park <dasol.park615@gmail.com>
2020-02-04 16:14:55 +0900

    Join, Log In with Social-Log In

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Sugaseong Church Responsive Website
+
+HTML5, CSS4, JavaScript, MongoDB, Babel, Webpack
+
+## Pages:
+
+### Global
+- [x] Home
+
+### User
+- [x] Join
+- [x] Login
+- [ ] Logout(Redirection to Home)
+- [ ] User(Redirection to userDetail)
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+
+### About
+- [ ] About(Redirection to Intro)
+- [ ] Intro
+
+### Time Table
+- [ ] Time Table
+
+### Board
+- [ ] Board
+- [ ] Search Board
+- [ ] Write Board
+- [ ] Board Detail
+- [ ] Edit Board
+- [ ] Delete Board
+
+## Map
+- [ ] Map
--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,11 +1,20 @@
 export const board = (req, res) => res.render('board', { pageTitle: '게시판' });
-export const searchBoard = (req, res) =>
+
+export const searchBoard = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
   res.render('search-board', { pageTitle: '게시판 검색' });
+};
+
 export const writeBoard = (req, res) =>
   res.render('writeBoard', { pageTitle: '게시판 작성' });
+
 export const boardDetail = (req, res) =>
   res.render('boardDetail', { pageTitle: '게시판 내용' });
+
 export const editBoard = (req, res) =>
   res.render('editBoard', { pageTitle: '게시판 수정' });
+
 export const deleteBoard = (req, res) =>
   res.render('deleteBoard', { pageTitle: '게시판 삭제' });
