e073b5a27f22a5d1e1a8084d4e6533724c9c7752
Dasol Park <dasol.park615@gmail.com>
2020-02-07 18:00:44 +0900

    [Styling] Map

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,2 @@
 import './boardIndex';
+import './map';
---
+++ src/assets/js/map.js
@@ -0,0 +1,11 @@
+const mapOptions = {
+  center: new naver.maps.LatLng(37.37363280609266, 126.96297172924932),
+  zoom: 18
+};
+
+const map = new naver.maps.Map('map', mapOptions);
+
+const marker = new naver.maps.Marker({
+  position: new naver.maps.LatLng(37.37363280609266, 126.96297172924932),
+  map: map
+});
