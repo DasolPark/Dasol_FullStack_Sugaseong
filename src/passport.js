0ad9c33221fb12dc5e1962ac1f80cc9ca25bf970
Dasol Park <dasol.park615@gmail.com>
2020-02-11 17:20:39 +0900

    Push config to heroku

--- src/passport.js
+++ src/passport.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 import passport from 'passport';
 import GithubStrategy from 'passport-github';
-// import FacebookStrategy from 'passport-facebook';
+import FacebookStrategy from 'passport-facebook';
 import User from './models/User';
 import {
-  githubLoginCallback
-  // facebookLoginCallback
+  githubLoginCallback,
+  facebookLoginCallback
 } from './controllers/userController';
 import routes from './routes';
 
@@ -26,20 +26,20 @@ passport.use(
   )
 );
 
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: process.env.FACEBOOK_CLIENT_ID,
-//       clientSecret: process.env.FACEBOOK_SECRET,
-//       callbackURL: process.env.PRODUCTION
-//         ? `https://still-hollows-17834.herokuapp.com/user${routes.facebookCallback}`
-//         : `http://localhost:4000/user${routes.facebookCallback}`,
-//       profileFields: ['id', 'displayName', 'photos', 'email'],
-//       scope: ['public_profile', 'email']
-//     },
-//     facebookLoginCallback
-//   )
-// );
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: process.env.PRODUCTION
+        ? `https://still-hollows-17834.herokuapp.com/user${routes.facebookCallback}`
+        : `http://localhost:4000/user${routes.facebookCallback}`,
+      profileFields: ['id', 'displayName', 'photos', 'email'],
+      scope: ['public_profile', 'email']
+    },
+    facebookLoginCallback
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
